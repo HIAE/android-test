657e34ce207038bb97bb88e6efd06c812be10503
rnd <rnd@outlook.com>
2020-03-15 13:59:06 -0300

    adiciona urls da api e permissões

---
+++ app/src/main/java/com/example/chuckeinstein/core/Constantes.kt
@@ -0,0 +1,10 @@
+package com.example.chuckeinstein.core
+
+object Constantes {
+    object URL_API_CHUCKNORRIS {
+        const val URL_BASE = "https://api.chucknorris.io/jokes/"
+        const val MODIFICADOR_CATEGORIAS = "categories"
+        const val MODIFICADOR_ALEATORIO_POR_CATEGORIA = "random?category="
+        const val URL_MODIFICADOR_BUSCA_POR_TEXTO = "search?query="
+    }
+}
