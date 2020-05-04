d9b125ca8abc50538e265cdcc47eaa3c2fd4a663
Antonio Carlos Del Grande Junior <antoniocgrande@gmail.com>
2020-05-04 18:29:38 -0300

    ChuckNoia-TestAA:: --created presentation (activities/, fragments/); --created (android.arch) navigation API;

---
+++ chuck_noia/app/src/main/java/br/com/antoniocgrande/chucknoia/presentation/activities/HomeActivity.kt
@@ -0,0 +1,15 @@
+package br.com.antoniocgrande.chucknoia.presentation.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import br.com.antoniocgrande.chucknoia.R
+
+class HomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+
+
+    }
+}
---
+++ chuck_noia/app/src/main/java/br/com/antoniocgrande/chucknoia/presentation/fragments/CategoriesFragment.kt
@@ -0,0 +1,19 @@
+package br.com.antoniocgrande.chucknoia.presentation.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import br.com.antoniocgrande.chucknoia.R
+
+class CategoriesFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_categories, container, false)
+    }
+
+}
