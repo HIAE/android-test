74796c0d5f3d0aab47b5bc7598273a148bb49456
renanparis <paris.oliveira@gmail.com>
2020-07-18 15:03:20 -0300

    Add koin modules to Random Joke Use Case

--- chuckjokes/src/main/java/com/renanparis/chuckjokes/di/AppModules.kt
+++ chuckjokes/src/main/java/com/renanparis/chuckjokes/di/AppModules.kt
@@ -4,7 +4,9 @@ import com.renanparis.chuckjokes.data.api.JokeWebClient
 import com.renanparis.chuckjokes.data.repository.JokeRepository
 import com.renanparis.chuckjokes.ui.adapter.CategoriesAdapter
 import com.renanparis.chuckjokes.ui.viewmodel.CategoryViewModel
+import com.renanparis.chuckjokes.ui.viewmodel.RandomJokeViewModel
 import com.renanparis.chuckjokes.usecase.CategoryUseCase
+import com.renanparis.chuckjokes.usecase.RandomJokeUseCase
 import org.koin.android.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
@@ -16,8 +18,10 @@ val dataModule = module {
     single<JokeWebClient> { JokeWebClient() }
     single<JokeRepository> { JokeRepository(get()) }
     single<CategoryUseCase> { CategoryUseCase(get()) }
+    single<RandomJokeUseCase> { RandomJokeUseCase(get()) }
 }
 
 val viewModelModule = module {
 viewModel<CategoryViewModel>{ CategoryViewModel(get()) }
+viewModel<RandomJokeViewModel>{ RandomJokeViewModel(get()) }
 }
