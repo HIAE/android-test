19103fec8c37c3d9a045f0801519950f99e4e4d9
renanparis <paris.oliveira@gmail.com>
2020-07-18 11:07:49 -0300

    Add layout list categories

--- chuckjokes/src/main/java/com/renanparis/chuckjokes/CategoriesActivity.kt
+++ chuckjokes/src/main/java/com/renanparis/chuckjokes/CategoriesActivity.kt
@@ -3,10 +3,10 @@ package com.renanparis.chuckjokes
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class CategoriesActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_categories)
     }
 }
